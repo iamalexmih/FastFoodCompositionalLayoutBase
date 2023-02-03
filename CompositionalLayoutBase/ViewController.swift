c30d01dc12f129355acd58e78e1258bf25580308
Alex <alexmihgel@gmail.com>
2023-02-03 21:05:53 +0300

    Initial Commit

---
+++ CompositionalLayoutBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutBase
+//
+//  Created by Алексей Попроцкий on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
